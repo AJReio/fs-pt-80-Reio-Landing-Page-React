aeec2af90ee850e29f38fad263272727f0030d95
AJReio <alejandrorello@gmail.com>
2024-10-23 16:45:51 +0000

    Corrección de texto en el footer

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -5,7 +5,7 @@ export const FooterComp = () =>{
     return (
         <div className="container-fluid card-footer bg-dark text-secondary d-flex justify-content-center p-4 flex-column">
         <p className= "align-self-center">Primero odiaréis react, pero pronto será vuestro mejor amigo. </p>
-        <p className="fst-italic align-self-center">Javier seiglie, 4GeeksAcademy 2024.</p>
+        <p className="fst-italic align-self-center">Javier Seiglie, 4GeeksAcademy 2024.</p>
    </div>
     )
 }
